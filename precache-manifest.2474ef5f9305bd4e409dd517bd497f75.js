ee5d6b97ec4f4fc2053268307662da52a348a16d
Ankan <ankanroy7454@gmail.com>
2022-12-11 14:37:48 +0530

    Initial commit

---
+++ precache-manifest.2474ef5f9305bd4e409dd517bd497f75.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "be290f05440f17682c29",
+    "url": "./static/css/main.23e0c7a4.chunk.css"
+  },
+  {
+    "revision": "be290f05440f17682c29",
+    "url": "./static/js/main.be290f05.chunk.js"
+  },
+  {
+    "revision": "002a2879119e0b5d31fb",
+    "url": "./static/js/1.002a2879.chunk.js"
+  },
+  {
+    "revision": "e43ab6eac0564d607fe1",
+    "url": "./static/css/2.b53f4428.chunk.css"
+  },
+  {
+    "revision": "e43ab6eac0564d607fe1",
+    "url": "./static/js/2.e43ab6ea.chunk.js"
+  },
+  {
+    "revision": "c1ce3d8d9f0161227288",
+    "url": "./static/js/runtime~main.c1ce3d8d.js"
+  },
+  {
+    "revision": "d27efe64a9dcc10ab1c32db6db5752f0",
+    "url": "./index.html"
+  }
+];
