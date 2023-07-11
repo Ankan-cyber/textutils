b76e7d8582a5e9d235039c99ad12c97ced6ee8b2
Ankan Roy <ankan@ankanroy.in>
2023-07-11 22:45:48 +0530

    added Find&Replace

---
+++ precache-manifest.b0c8b04725d4514b215ed4c6d4397fa6.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "14dd2caf67fd68e6dde2",
+    "url": "/textutils/static/css/main.02c6df6b.chunk.css"
+  },
+  {
+    "revision": "14dd2caf67fd68e6dde2",
+    "url": "/textutils/static/js/main.14dd2caf.chunk.js"
+  },
+  {
+    "revision": "002a2879119e0b5d31fb",
+    "url": "/textutils/static/js/1.002a2879.chunk.js"
+  },
+  {
+    "revision": "0dada9935842e5ae2841",
+    "url": "/textutils/static/css/2.95c99159.chunk.css"
+  },
+  {
+    "revision": "0dada9935842e5ae2841",
+    "url": "/textutils/static/js/2.0dada993.chunk.js"
+  },
+  {
+    "revision": "49f8ca36a88c13f66279",
+    "url": "/textutils/static/js/runtime~main.49f8ca36.js"
+  },
+  {
+    "revision": "db39e00782b388d390df78a155dd4218",
+    "url": "/textutils/index.html"
+  }
+];
